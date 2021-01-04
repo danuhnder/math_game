9a03de80e8aaf925a8091755dc170644817e4372
Daniel Robertson <danieljrobertson@gmail.com>
2021-01-04 17:11:15 +0000

    build out some of game logic

---
+++ math_game.rb
@@ -0,0 +1,22 @@
+# Entities - Game, Questions, Players, Lives, Score
+
+#Game class - stores information about scores and who's turn it is. Has methods to
+  # track which players turn it is
+  #generate questions
+  # alter score
+
+  
+#Question class - initialised with two values and a player number
+  # outputs to stdout
+
+
+#Player class 
+  # holds while other players turn is happening 
+  # recieves user input methods though chomp
+
+# Game logic
+ #Set scores to 3 apiece
+ #gen question for player 1
+ #check answer and adjust score if necessary
+ #check if players score is zero
+ # gen question for player 2
